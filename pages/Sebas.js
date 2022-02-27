4f927dd035d64eecde8a06cd8ac8831e63a09700
Daniel Jaramillo <losteven2018@outlook.com>
2022-02-27 00:03:20 -0500

    Added new screens

---
+++ pages/Sebas.js
@@ -0,0 +1,14 @@
+import {View, Text} from 'react-native'
+import Container from '../src/Container'
+import Infinito from '../src/Infinito'
+
+const Sebas = () => {
+    return(
+        <Container>
+            <Infinito />
+            <Text>Hola, esta es la poderosísima página de Sebas</Text>
+        </Container>
+    )
+}
+
+export default Sebas
